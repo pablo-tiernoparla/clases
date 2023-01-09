8198ea0df85f3156f5f4a8a2169c4c16076d0e40
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2023-01-09 17:50:30 +0100

    clase hora

---
+++ src/main/java/daw/programacion/clases/Hora.java
@@ -0,0 +1,59 @@
+package daw.programacion.clases;
+
+public class Hora {
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Hora(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return this.hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return this.minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return this.second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+    
+    public void setTime(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    @Override
+    public String toString(){
+        return this.hour + ":" + this.minute + ":" + this.second;
+    }
+
+    public String nextSecond(){
+        second++;
+        return this.hour + ":" + this.minute + ":" + this.second;
+    }
+
+    public String previousSecond(){
+        second--;
+        return this.hour + ":" + this.minute + ":" + this.second;
+    }
+}
