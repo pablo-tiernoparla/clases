39c04f6f24ba53c59c9c9fbfa78aa3453dccd9b5
pablo-tiernoparla <pablo.delgadosoto@tiernoparla.es>
2023-01-13 21:20:46 +0100

    excceptions class fecha

--- src/main/java/daw/programacion/clases/Fecha.java
+++ src/main/java/daw/programacion/clases/Fecha.java
@@ -1,15 +1,19 @@
 package daw.programacion.clases;
 
 public class Fecha {
-    
+
     private int day;
     private int month;
     private int year;
 
-    public Fecha(int day, int month, int year){
-        this.day = day;
-        this.month = month;
-        this.year = year;
+    public Fecha(int day, int month, int year) throws IllegalArgumentException{
+        if (1 > day && day > 31 && 1 > month && month > 12 && 1998 > year && year > 9999) {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        } else {
+            throw new IllegalArgumentException("numeros invalidos");
+        }
     }
 
     public int getDay() {
@@ -24,35 +28,42 @@ public class Fecha {
         return this.year;
     }
 
-    public void setDay(int day) {
-        if (day > 0 && day < 32){
+    public void setDay(int day) throws IllegalArgumentException {
+        if (day > 0 && day < 32) {
             this.day = day;
+        } else {
+            throw new IllegalArgumentException(day + "must be between 1 and 31");
         }
-        throw new Exception("day must be between 1 and 31");
     }
 
-    public void setMonth(int month) {
-        if (month > 0 && month < 13){
+    public void setMonth(int month) throws IllegalArgumentException {
+        if (month > 0 && month < 13) {
             this.month = month;
+        } else {
+            throw new IllegalArgumentException(month + "must be between 1 and 12");
         }
-        throw new datoMalAsignado(month + "must be between 1 and 12");
     }
 
-    public void setYear(int year) {
-        if (year > 1900 && year < 9999){
+    public void setYear(int year) throws IllegalArgumentException {
+        if (year > 1900 && year < 9999) {
             this.year = year;
+        } else {
+            throw new IllegalArgumentException(year + "must be between 1900 and 9999");
         }
-        throw new datoMalAsignado(year + "must be between 1900 and 9999");
     }
 
-    public void setDate(int day, int month, int year){
-        this.day = day;
-        this.month = month;
-        this.year = year;
+    public void setDate(int day, int month, int year) throws IllegalArgumentException {
+        if (1 > day && day > 31 && 1 > month && month > 12 && 1998 > year && year > 9999) {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        } else {
+            throw new IllegalArgumentException("numeros invalidos");
+        }
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return this.day + "/" + this.month + "/" + this.year;
-    }    
+    }
 }
